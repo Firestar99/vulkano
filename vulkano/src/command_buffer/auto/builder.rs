5741d0182e0e4c45c7ddae9bee895ea453a3dfc7
Firestar99 <4696087-firestar99@users.noreply.gitlab.com>
2024-06-27 22:21:58 +0200

    don't accumulate empty direct arrays

--- vulkano/src/command_buffer/auto/builder.rs
+++ vulkano/src/command_buffer/auto/builder.rs
@@ -542,11 +542,16 @@ impl<'a> MergedCommandInfo<'a> {
         } else {
             (None, SmallVec::new())
         };
+        let direct = if value.used_resources.direct.is_empty() {
+            SmallVec::new()
+        } else {
+            SmallVec::from_iter([&value.used_resources.direct])
+        };
         Self {
             name: value.name,
             render_pass: value.render_pass,
             pipeline,
-            direct: SmallVec::from_iter([&value.used_resources.direct]),
+            direct,
             deferred,
         }
     }
@@ -561,6 +566,7 @@ impl<'a> MergedCommandInfo<'a> {
             return Err(b);
         }
 
+        // success
         if self.pipeline == None {
             self.pipeline = b.pipeline;
         }
